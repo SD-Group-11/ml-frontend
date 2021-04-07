91624e28045719b3fae19158ce8e09c49e675b9f
Blip-On-Git-2-Learn <69735970+Blip-On-Git-2-Learn@users.noreply.github.com>
2021-04-07 16:55:04 +0200

    Update test_login.py

--- test/test_login.py
+++ test/test_login.py
@@ -2,13 +2,13 @@ from io import StringIO
 import pytest
 import login
 
-tAdmin=StringIO("admi")
+tAdmin=StringIO("admin")
 tPass=StringIO("password")
 
 def test_enterUsername(monkeypatch):
     monkeypatch.setattr('sys.stdin', tAdmin)
     assert login.enterUsername()=="admin"
 
-#def test_enterPassword(monkeypatch):
- #   monkeypatch.setattr('sys.stdin', tPass)
-  #  assert login.enterPassword()=="password"
+def test_enterPassword(monkeypatch):
+    monkeypatch.setattr('sys.stdin', tPass)
+    assert login.enterPassword()=="password"
