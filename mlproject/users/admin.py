ab06993c6807bf98f04a7b5dddcc40dec1dd479a
1827555 <69573834+1827555@users.noreply.github.com>
2021-04-24 22:40:10 +0200

    Delete mlproject directory

--- mlproject/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User
-# Register your models here.
-
-## Just add this is the respective admin file of the app you create so we can use it 
-admin.site.register(User)
