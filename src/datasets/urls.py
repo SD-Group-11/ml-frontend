3f102eae5f5efedcbce8df25fd041b3d5ffe574b
1827555@students.wits.ac.za <Yaseen.Haffejee1@students.wits.ac.za>
2021-06-06 18:06:03 +0200

    Fixed backend

--- src/datasets/urls.py
+++ src/datasets/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 
 from .views import(
     receiveData,
-    dataSummary
+    getDatasetsInfo,
+    getDatasetData,
+    doLinearRegression
 ) 
 
 app_name ='datasets'
 ## we're just creating the urls the data should be sent to for the respective processes in the view
 urlpatterns =[
     path('uploadData',receiveData,name='receivedata'),
-    path('dataSummary',dataSummary,name='datasummary')
+    path('getDatasetsInfo',getDatasetsInfo,name='getDatasetsInfo'),
+    path('getDatasetData',getDatasetData,name='getDatasetData'),
+    path('doLinearRegression',doLinearRegression,name='doLinearRegression')
+    
 ]
