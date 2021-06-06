6ae5837cb78336a8e7525ed64619bebdc0db4a40
jemmooo <1598024@students.wits.ac.za>
2021-06-06 23:54:35 +0200

    slightly more updated file

--- shell_vue/src/views/mlModels/LinearRegressionDatasets.vue
+++ shell_vue/src/views/mlModels/LinearRegressionDatasets.vue
@@ -7,9 +7,11 @@
             </div>
         </section>
 
-        <div class="column is-multiline">
+        <section class="section">
+            <div class="container">
+                <div class="column is-multiline">
 
-            <div class="column">    
+                    <div class="column">    
 
                 <div class="columns is-multiline is-mobile">
 
@@ -28,45 +30,126 @@
                                                 <input class="file-input" id="myFile" type="file" accept=".csv"  v-on:input="fileValidation">
                                                     
                                                     <div class="file-cta">
-                                                        <div class="file-label" >
-                                                            Choose a file…
+                                                                <div class="file-label" >
+                                                                    Choose a file…
+                                                                </div>
+                                                            </div>
+
+                                                        <div class="file-name">  
+                                                            {{uploadedName}} 
                                                         </div>
-                                                    </div>
-
-                                                <div class="file-name">  
-                                                    {{uploadedName}}    
+                                                    </label>
                                                 </div>
-
-                                            </label>
+                                            </div>
                                         </div>
-                                    </div>
+
+                                        <div class="field">
+                                            <div class="control">
+                                                <button id = 'uploadFile' v-if="uploadable" type="submit" v-on:click = 'Upload' class="button is-info has-text-black"><strong>Submit</strong></button>
+                                            </div>
+                                        </div>
+
+                                    </form>
                                 </div>
                                 
 
-                                <div class="field">
-                                    <div class="control">
-                                        <button id = 'uploadFile' v-if="uploadable" type="submit" v-on:click = 'Upload' class="button is-info has-text-black"><strong>Submit</strong></button>
-                                    </div>
+                            </div>
+                        
+                            <div class="column is-half">
+                                <div class="box" style="background-color:lightyellow;" >
+                                    <h2 class="title is-3">Uploaded data</h2>
+                                    <p id="data" ref="para"> Metadata will appear here
+                                    </p>
                                 </div>
 
-                            </form>
+                            </div>
                         </div>
-
-                    </div>
-                
-                    <div class="column is-half">
                         
-                        <div class="box" style="background-color:lightyellow;" >
-                            <h2 class="title is-3">Uploaded data</h2>
-                            <p id="data" ref="para"> Metadata will appear here
-                            </p>
-                        </div>
-
                     </div>
                 </div>
-                
             </div>
-        </div>
+        </section>
+
+
+        <section class="hero is-light">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">Expandable Bulma table rows CSS & HTML</h1>
+                    <h3 class="subtitle">Check desktop & mobile layout</h3>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <div class="b-table">
+                <div class="table-wrapper has-mobile-cards">
+                    <table class="table is-fullwidth is-hoverable is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th class="is-chevron-cell"></th>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Created</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td class="is-chevron-cell">
+                            <a role="button">
+                                <span class="icon is-expanded"><i class="fas fa-angle-right"></i></span>
+                            </a>
+                        </td>
+                        <td class="is-image-cell">
+                            <div class="image">
+                                <!-- <img src="@/assets/images/confused-icon-6-yellow.png"> -->
+                            </div>
+                        </td>
+                        <td data-label="Name">Rebecca Bauch</td>
+                        <td data-label="Created">
+                            <small class="has-text-grey is-abbr-like" title="Oct 25, 2020">Oct 25, 2020</small>
+                        </td>
+                        <td class="is-actions-cell">
+                            <div class="buttons is-right">
+                                <button class="button is-small is-primary" type="button">
+                                    <span class="icon"><i class="mdi mdi-eye"></i></span>
+                                </button>
+                                <button class="button is-small is-danger jb-modal" data-target="sample-modal" type="button">
+                                    <span class="icon"><i class="mdi mdi-trash-can"></i></span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="detail">
+                        <td colspan="8">
+                        <div class="detail-container">
+                            <article class="media">
+                            <figure class="media-left">
+                                <div class="image is-64x64">
+                                    <!-- <img src="@/assets/images/csv-icon/128x128.png"> -->
+                                </div>
+                            </figure>
+                            <div class="media-content">
+                                <div class="content">
+                                <p><strong>Rebecca Bauch</strong> <small>@rbauch</small> <small>1d</small><br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    Proin ornare magna eros, eu pellentesque tortor vestibulum ut.
+                                    Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+                                </p>
+                                </div>
+                            </div>
+                            </article>
+                        </div>
+                        </td>
+                    </tr>
+                    </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
+        </section>
+
     </div>
 </template>
 
@@ -90,6 +173,9 @@
                 SummaryData: [],
                 uploadedName: '',
                 uploadable: false,
+                datasetDetails: [],
+                uploadedFilename: '',
+                hasDatasets: false,
             }
         },
         mounted(){
@@ -104,7 +190,7 @@
 
                     .then(response => {
                         this.userDetails=response.data
-                        console.log(response)
+                        this.getUserDatasets()
                     })
 
                     .catch(error => {
@@ -114,13 +200,50 @@
                 this.$store.commit('setIsLoading',false)
 
             },
+            async getUserDatasets(){
+                this.$store.commit('setIsLoading',true)
+                
+                var data ={"UserId":this.userDetails.id}
+                await axios
+                .post('/datasets/getDatasetsInfo',data)
+                
+                .then(response =>{
+                    //idk why but accessing UserFiles out of this scope returns empty. Please check what im doing wrong
+                    // response.data though holds all the datasets of a user and their respective summary details
+                    this.UserFiles = response.data;
+                    // Tell us how many datasets are associated with the user 
+                    // you can loop from 1 to number_of_datasets+1 and use that to index response.data[i] to get a dataset and its summary
+                    
+                    if(!response.data['error']=="No datasets have been uploaded."){
+                        var number_of_datasets = Object.keys(response.data).length
+
+                        for(var i=1;i<number_of_datasets+1;i++){
+                        
+                        console.log(response.data[i])
+                        //Do whatever with each dataset
+                        //reponse.data[i].anything below will give you it's value
+                        //filename gives filename
+                        //datapoints gives number of datapoints
+                        //columns give number of columns
+                        //featureNames gives an array of all the features 
+                        //nullValues gives how many nulls in the dataset
+                    
+                        }
+                    }
+                    
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+
+                this.$store.commit('setIsLoading',false)
+            },
             async Upload(){
                 this.$store.commit('setIsLoading',true)
                 var file = document.getElementById("myFile").files[0];
                 var formData = new FormData();
                 formData.append("dataset",file);
-                formData.append("id",this.details.id);
-                console.log(this.details.id);
+                formData.append("id",this.userDetails.id);
                 await axios
                     .post('/datasets/uploadData',formData)
 
@@ -130,17 +253,19 @@
                         if (resp == 'Data Uploaded Successfully'){
                             Type = 'is-warning';
                             this.uploaded=true;
-                            var filename = file.name;
-                            var id=  this.details.id;
-                            var data ={"id":id,"filename":filename}
-
-                            axios
-                            .post('/datasets/dataSummary',data)
+                            this.getUserDatasets()
                             
-                            .then(response =>{
-                                this.SummaryData = response.data;
-                                this.$refs.para.innerText="File Name:"+ this.SummaryData.filename +"\nNumber of data points:"+this.SummaryData.datapoints+" \nNumber of Columns:"+this.SummaryData.columns;
-                            });
+                            // this.uploadedFilename = `"${file.name}"`;
+                            // var data = {'UserId':this.userDetails.id,"filename":this.uploadedFilename}
+                            // axios
+                            // .post("/datasets/getDatasetData",data)
+                            // .then(response => {
+                            //     //Store the response and use it to get converted into a csv file for download
+                            //     // First implement a check that the response is not "error: Failed to load dataset. Please try again"
+                            //     // That will happen if the backend fails to load the data of the selected file. 
+                            //     console.log(response.data)
+                            // })  
+                              
                         }
                         else{
                             this.uploaded=false;
@@ -158,25 +283,26 @@
 
                  this.$store.commit('setIsLoading',false)
             },
-
-            async Summary(){
-                this.$store.commit('setIsLoading',true)
-                
-                var file = document.getElementById("myFile").files[0];
-                var filename = file.name;
-                var id=  this.details.id;
-                var data ={"id":id,"filename":filename}
+            //This method will get the actual data of a dataset once it's selected from the dropdown list
+            async GetDatasetData(){
+                console.log(this.userDetails.id)
+                console.log(this.uploadedFilename)
+                var id =  this.userDetails.id;
+                //Please fill in the file name that will be sent once they have selected in from the dropdown
+                var filename ;
+                //var data = {'UserId':id,"filename":filename}
+                var data = {'UserId':id,"filename":this.uploadedFilename}
                 await axios
-                .post('/datasets/dataSummary',data)
-                
-                .then(response =>{
-                    this.SummaryData = response.data;
+                .post("/datasets/getDatasetData",data)
 
-                });
-                console.log(this.SummaryData);
-                this.$store.commit('setIsLoading',false)
+                .then(response => {
+                    //Store the response and use it to get converted into a csv file for download
+                    // First implement a check that the response is not "error: Failed to load dataset. Please try again"
+                    // That will happen if the backend fails to load the data of the selected file. 
+                    console.log(response.data)
+                })
+
             },
-
             async fileValidation(){
                 this.$store.commit('setIsLoading',true)
                 
@@ -219,7 +345,25 @@
                     this.$store.commit('setIsLoading',false)
                     return true;
                 }         
-            }
+            },
+            // async Summary(){
+            //     this.$store.commit('setIsLoading',true)
+                
+            //     var file = document.getElementById("myFile").files[0];
+            //     var filename = file.name;
+            //     var id=  this.details.id;
+            //     var data ={"id":id,"filename":filename}
+            //     await axios
+            //     .post('/datasets/dataSummary',data)
+                
+            //     .then(response =>{
+            //         this.SummaryData = response.data;
+
+            //     });
+            //     console.log(this.SummaryData);
+            //     this.$store.commit('setIsLoading',false)
+            // },
+
         }
     }
 </script>
