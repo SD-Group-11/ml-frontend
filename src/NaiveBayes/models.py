1a66915cda26ad397fec61a93c61738ee8ec4374
1827555@students.wits.ac.za <Yaseen.Haffejee1@students.wits.ac.za>
2021-09-14 12:23:27 +0200

    Naive Bayes

---
+++ src/NaiveBayes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.db.models import JSONField
+# Create your models here.
+
+#Class for storing the trained model after LR has been run. 
+# Uses a compound primary key of UserId and filename 
+class NaiveBayes(models.Model): 
+    UserId = models.IntegerField(unique=False,null=False)
+    filename = models.CharField(max_length=300)
+    TrainingAccuracy = JSONField(default=dict,null=True)
+    TestingAccuracy = models.DecimalField(max_digits=19,decimal_places=10,null=True)
+    AUCScore = models.DecimalField(max_digits=19,decimal_places=10,null=True)
+
+
+
+    class Meta:
+        unique_together = (('UserId', 'filename'),)
+
