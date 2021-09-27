11bce908d954b3ed3317f9d518fa9e117b7d1abf
1827555@students.wits.ac.za <Yaseen.Haffejee1@students.wits.ac.za>
2021-09-27 22:49:46 +0200

    fixed model and delete dataset

--- src/NaiveBayes/admin.py
+++ src/NaiveBayes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NaiveBayes
+from .models import NBTrainedModel
 # Register your models here.
 
-admin.site.register(NaiveBayes)
+admin.site.register(NBTrainedModel)
