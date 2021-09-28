4c56d135e63910e04d0b14b0ab6c3391bb578a18
phoenixgryphon11 <69591033+phoenixgryphon11@users.noreply.github.com>
2021-09-28 13:37:47 +0200

    Update LinearRegressionTest.spec.js

--- shell_vue/tests/unit/LinearRegressionTest.spec.js
+++ shell_vue/tests/unit/LinearRegressionTest.spec.js
@@ -1,4 +1,4 @@
-import LinearRegressionTest from '@/views/mlModels/LinearRegressionTest.vue'
+import LinearRegressionTest from '@/views/LinearRegressionTest.vue'
 import { createStore } from 'vuex'
 import { mount, flushPromises,  shallowMount  } from '@vue/test-utils'
 import axios from 'axios'
