1c8e3d9a49294570e456df1b757b71361c0f97b4
blipOnGit <talionnaidoo@gmail.com>
2021-10-19 15:11:15 +0200

    attempt 3 serve django static files in production

--- src/mlproject/settings.py
+++ src/mlproject/settings.py
@@ -110,11 +110,17 @@ TEMPLATES = [
 ]
 
 # serve django static files in production 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
