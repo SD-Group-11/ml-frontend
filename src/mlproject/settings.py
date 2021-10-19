fb569273bdc626ed7b074bb1d6293c2cb012e263
blipOnGit <talionnaidoo@gmail.com>
2021-10-19 14:48:34 +0200

    attempt to get django static files to deploy

--- src/mlproject/settings.py
+++ src/mlproject/settings.py
@@ -71,7 +71,6 @@ INSTALLED_APPS = [
     'LinearRegression',
     'NaiveBayes',
     'LogisticRegression',
-    
 ]
 
 MIDDLEWARE = [
@@ -106,10 +105,11 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'/static')
 ]
-STATIC_URL = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'mlproject.wsgi.application'
 
