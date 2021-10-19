e31cfa3bce69279d4fedfe54f73afad6ac113abc
blipOnGit <talionnaidoo@gmail.com>
2021-10-19 15:16:22 +0200

    Django static files served with whitenoise

--- src/mlproject/settings.py
+++ src/mlproject/settings.py
@@ -110,18 +110,11 @@ TEMPLATES = [
 ]
 
 # serve django static files in production 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'mlproject.wsgi.application'
