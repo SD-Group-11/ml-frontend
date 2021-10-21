8c88b11769e954fae0554e52b213f0f4160340e4
blipOnGit <talionnaidoo@gmail.com>
2021-10-21 11:33:32 +0200

    Fix sendgrid

--- src/mlproject/settings.py
+++ src/mlproject/settings.py
@@ -154,7 +154,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # EMAIL_HOST = env('EMAIL_HOST')
 # EMAIL_USE_TLS = env('EMAIL_USE_TLS')
-# EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_PORT = env('EMAIL_PORT')
 # EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 # EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
@@ -162,7 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # always use this
 EMAIL_HOST_PASSWORD = env('SENDGRID_API_KEY')
-EMAIL_PORT = 587
+# EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'mlfframework@gmail.com'
 
