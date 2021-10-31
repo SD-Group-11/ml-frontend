96c7d81ee5ce95bcb9d86c80910791651756d6e2
UreeshahMoodley <moodleyureeshah@gmail.com>
2021-10-31 15:55:21 +0200

    Done - getting started page

--- shell_vue/src/views/GettingStarted.vue
+++ shell_vue/src/views/GettingStarted.vue
@@ -8,23 +8,24 @@
 
     </div>
 
+
     <div class="container is-fluid p-2">
-        <div class="notification is-info is-light has-text-left" >
-            <h3 class="title is-4">Uploading</h3>
+        <div class="notification is-info is-light has-text-centered" >
+            <h3 class="title is-2">Uploading</h3>
         </div>
 
         <div>
-                  <ul class="steps is-vertical is-hollow is-dashed">
-              <li class="steps-segment">
-                <span href="#" class="steps-marker is-warning"></span>
+           <ul class="steps is-vertical is-hollow is-dashed ">
+              <li class="steps-segment is-active">
+                <span class="steps-marker is-warning"></span>
                 <div class="steps-content">
                   <!-- <code>is-primary</code> -->
                   <p class="is-size-4">Training dataset</p>
-                  <p>Use "Choose file" to select and upload your training dataset.</p>
+                  <p>Under your chosen model, use "Choose file" to select and upload your training dataset.</p>
                 </div>
               </li>
               <li class="steps-segment">
-                <span href="#" class="steps-marker is-warning"></span>
+                <span class="steps-marker is-warning"></span>
                 <div class="steps-content">
                   <p class="is-size-4">Associated training dataset</p>
                   <p>Upload the test dataset that is associated with your uploaded training dataset.</p>
@@ -35,7 +36,8 @@
                 <span class="steps-marker is-warning"></span>
                 <div class="steps-content ">
                   <p class="is-size-4">Dataset Options</p>
-                  <p>View, delete, download datasets.</p>
+                  <p>Download, view or delete datasets.</p>
+                  <img src="@/assets/images/upload2NEW.png" alt="Logo" width="1200" height="1200">
                   <!-- add picture -->
                 </div>
               </li>
@@ -44,32 +46,33 @@
     </div>
 
     <div class="container is-fluid p-2">
-        <div class="notification is-link is-light has-text-left" >
-            <h3 class="title is-4">Training</h3>
+        <div class="notification is-link is-light has-text-centered" >
+            <h3 class="title is-2">Training</h3>
         </div>
 
         <div>
                   <ul class="steps is-vertical is-hollow is-dashed">
-              <li class="steps-segment">
-                <span href="#" class="steps-marker is-warning"></span>
+              <li class="steps-segment is-active">
+                <span class="steps-marker is-warning"></span>
                 <div class="steps-content">
                   <!-- <code>is-primary</code> -->
-                  <p class="is-size-4">2000</p>
-                  <p>Lorem Ipsum Dolor Sit Amet</p>
-                </div>
-              </li>
-              <li class="steps-segment">
-                <span href="#" class="steps-marker is-warning"></span>
-                <div class="steps-content">
-                  <p class="is-size-4">2002</p>
-                  <p>Maecenas Ultrices Aliquet Velit Vitae</p>
+                  <p class="is-size-4">Select a dataset</p>
+                  <p>Use the dropdown to select a dataset to train.</p>
                 </div>
               </li>
               <li class="steps-segment">
                 <span class="steps-marker is-warning"></span>
                 <div class="steps-content">
-                  <p class="is-size-4">2007</p>
-                  <p>Aliquam Quis Scelerisque Mauris Nullam</p>
+                  <p class="is-size-4">Train</p>
+                  <p>Click the "Train Model" button to display results.</p>
+                </div>
+              </li>
+              <li class="steps-segment">
+                <span class="steps-marker is-warning"></span>
+                <div class="steps-content">
+                  <p class="is-size-4">Results</p>
+                  <p>Results will be displayed for you to interpret, download or discard.</p>
+                  <img src="@/assets/images/training2.png" alt="Logo" width="1200" height="1200" >
                 </div>
               </li>
               </ul>
@@ -77,38 +80,70 @@
     </div>
 
     <div class="container is-fluid p-2">
-        <div class="notification is-link is-light has-text-left" >
-            <h3 class="title is-4">Testing</h3>
+        <div class="notification is-link is-light has-text-centered" >
+            <h3 class="title is-2">Testing</h3>
         </div>
 
         <div>
             <ul class="steps is-vertical is-hollow is-dashed">
-              <li class="steps-segment">
-                <span href="#" class="steps-marker is-warning"></span>
+              <li class="steps-segment is-active">
+                <span class="steps-marker is-warning"></span>
                 <div class="steps-content">
                   <!-- <code>is-primary</code> -->
-                  <p class="is-size-4">2000</p>
-                  <p>Lorem Ipsum Dolor Sit Amet</p>
-                </div>
-              </li>
-              <li class="steps-segment">
-                <span href="#" class="steps-marker is-warning"></span>
-                <div class="steps-content">
-                  <p class="is-size-4">2002</p>
-                  <p>Maecenas Ultrices Aliquet Velit Vitae</p>
+                  <p class="is-size-4">Select a trained model</p>
+                  <p>Use the dropdown to select the trained model set you want test. Testing can only be done on a trained model.</p>
                 </div>
               </li>
               <li class="steps-segment">
                 <span class="steps-marker is-warning"></span>
                 <div class="steps-content">
-                  <p class="is-size-4">2007</p>
-                  <p>Aliquam Quis Scelerisque Mauris Nullam</p>
+                  <p class="is-size-4">Testing data</p>
+                  <p>If there is no available test data, use the button on the testing page to upload one.</p>
+                  <img src="@/assets/images/test.png" alt="Logo" width="10" height="10" class="center">
+                </div>
+              </li>
+              <li class="steps-segment">
+                <span class="steps-marker is-warning"></span>
+                <div class="steps-content">
+                  <p class="is-size-4">Test</p>
+                  <p>Click the "Test Model" button to display results.</p>
+                </div>
+              </li>
+              <li class="steps-segment">
+                <span class="steps-marker is-warning"></span>
+                <div class="steps-content">
+                  <p class="is-size-4">Results</p>
+                  <p>Results will be displayed for you to interpret, download or discard just like training results.</p>
                   <!-- <img src="@/assets/images/confused-icon-6-yellow.png" alt="Logo"> -->
                   <!-- can add images hooray :) -->
                 </div>
               </li>
               </ul>
         </div>
+
+
+  <!-- REUSING -->
+    <!-- <div>
+   <ul class="steps has-content-centered">
+          <li class="steps-segment is-active is-dashed">
+            <span class="steps-marker is-warning">1</span>
+            <div class="steps-content"> --> 
+              <!-- <p class="is-size-4">Step 1</p> -->
+              <!-- <p class="is-size-5">Select an algorithm</p>
+            </div>
+          </li>
+
+          <li class="steps-segment is-active is-dashed">
+            <span class="steps-marker is-warning">2</span>
+            <div class="steps-content">  -->
+              <!-- <p class="is-size-4">Step 2</p> -->
+              <!-- <p class="is-size-5">Upload a dataset</p>
+            </div>
+          </li>
+        </ul>
+  </div> -->
+
+       
 
     </div>
 
@@ -393,6 +428,21 @@
     margin-left: auto;
     margin-right: auto;
   }
+
+  img 
+  { 
+    padding: 15px;
+    /* max-width:200%;
+    height:200%; */
+    /* resizing robot for landing page */
+  }
+
+  .center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
 
   
 </style>
