26db25bf1a23975a4be3199cd777a66ab4064738
jemmooo <41875987+jemmooo@users.noreply.github.com>
2021-11-08 11:01:59 +0200

    Update jest.config.js

--- shell_vue/jest.config.js
+++ shell_vue/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!ag-grid-vue)']
 }
