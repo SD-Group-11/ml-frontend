dd41dedd26ec9e89fb3d1a11b0fe6c25d256e1c1
jemmooo <1598024@students.wits.ac.za>
2021-11-09 21:32:03 +0200

    changes so far

--- shell_vue/src/components/layout/Footer.vue
+++ shell_vue/src/components/layout/Footer.vue
@@ -4,8 +4,22 @@
       <div class = "container">
 
         <div class = "columns">
-          <div class = "column is-full has-text-grey has-text-centered">
-            <p><i>Brought to you by : <strong>IllegalSkillsException</strong>.</i></p>
+         <div class = "column is-full has-text-dark-link has-text-centered">
+            <p>
+              <i>
+              Brought to you by
+               <strong>IllegalSkillzException
+               <br/>
+
+               Accomplished: None |
+               Impediments: None |
+               Plan to: None
+
+
+
+              </strong>
+              </i>
+              </p>
           </div>
         </div>
 
--- shell_vue/src/views/FAQ.vue
+++ shell_vue/src/views/FAQ.vue
@@ -29,9 +29,9 @@
                 </button>
               </div>
               <transition name="dropdown">
-              <div class="message-body" v-show='toggle'>
+              <div class="message-body" v-if ="toggle" @click="toggle =!toggle">
                 <div class="notification is-info  is-light">
-                  You can reset your password from the sign-in form.
+                 You can reset your password from the sign-in form.
                 </div>
               </div>
               </transition>     
@@ -51,7 +51,7 @@
               <transition name="dropdown">
               <div class="message-body" v-show='toggle1'>
                 <div class="notification is-info  is-light">
-                  If you have not received an email to change your password try checking your spam folder. Otherwise please contact mlfframework@gmail.com.
+                  If you have not received an email to change your password try checking your spam folder. Otherwise please contact leftframe.ml@gmail.com.
 
                 </div>
                 </div>
@@ -114,7 +114,7 @@
               </div>
                <transition name="dropdown">
               <div class="message-body" v-show='toggle4'>
-                <div class="notification is-info  is-light ml-5">
+               <div class="notification is-info is-light ml-0">
                   <ol type="1">
                     <li>Click on "Choose File".</li>
                     <li>Select a .csv file from your local device.</li>
@@ -146,7 +146,49 @@
                </transition>
             </article>
 
-            
+             <!-- new FAQ -->
+            <article class="message is-warning">
+              <div class="message-header">
+                <p> Why can't I upload my datasets to public datasets?
+                </p>
+
+                <button class="button is-warning is-light" @click="toggle6= !toggle6">
+                  <span class="icon is-small">
+                    <i class="fas fa-chevron-circle-down"></i>
+                  </span>
+                </button>
+              </div>
+               <transition name="dropdown">
+              <div class="message-body" v-show='toggle6'>
+                <div class="notification is-info  is-light">
+                    In order or your dataset to show up as public you need to ensure that you have uploaded
+                    test data with the associated dataset
+                </div>
+              </div>
+               </transition>
+            </article>
+
+            <!-- new FAQ -->
+            <article class="message is-warning">
+              <div class="message-header">
+                <p> I don't have a dataset. How can I try out LeftFrame?
+                </p>
+
+                <button class="button is-warning is-light" @click="toggle7= !toggle7">
+                  <span class="icon is-small">
+                    <i class="fas fa-chevron-circle-down"></i>
+                  </span>
+                </button>
+              </div>
+               <transition name="dropdown">
+              <div class="message-body" v-show='toggle7'>
+                <div class="notification is-info  is-light">
+                    If you don't have a dataset to work with and would like to try out one of the models you can simply download
+                    a dataset from the public datasets page made available to all users by other users of the site   
+                </div>
+              </div>
+               </transition>
+            </article>
 
           </div>
  
@@ -193,7 +235,7 @@
                <transition name="dropdown">
               <div class="message-body" v-show='toggle1'>
                 <div class="notification is-info  is-light">
-                  If you have not received an email to change your password try checking your spam folder. Otherwise please contact mlfframework@gmail.com.
+                  If you have not received an email to change your password try checking your spam folder. Otherwise please contact leftframe.ml@gmail.com.
 
                 </div>
                 </div>
@@ -249,22 +291,27 @@
   }
 
 
- .dropdown-enter,
- .dropdown-leave-to {
-  transform: scaleY(0.7);
+ .dropdown-enter-from{
   opacity: 0;
- }
- 
- .dropdown-enter-to,
- .dropdown-leave {
+  transform: scaleY(0.5);
+}
+.dropdown-enter-to{
   opacity: 1;
   transform: scaleY(1);
- }
- 
- .dropdown-enter-active,
- .dropdown-leave-active {
+}
+.dropdown-enter-active{
+  transition: all 0.3s ease-in;
+}
+.dropdown-leave-from{
+  opacity: 1;
+  transform: scaleY(1);
+}
+.dropdown-leave-to{
+  opacity: 0;
+  transform: scaleY(0.5);
+  }
+.dropdown-leave-active{
   transition: all 0.3s ease-out;
-  transform-origin: top center;
 }
 </style>
 
@@ -281,6 +328,8 @@ export default {
       toggle3: false,
       toggle4: false,
       toggle5: false,
+      toggle6: false,
+      toggle7: false,
     }
   }
 }
